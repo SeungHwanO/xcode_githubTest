33e028bee325cb7591deb420502b6fa99eba55c5
SeungHwanO <pro5kinghwan@gmail.com>
2021-12-24 14:51:08 +0900

    git commit 테스트

--- xcode_githubTest/ViewController.swift
+++ xcode_githubTest/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
         print("hi")
         print("github")
+        print("git 커밋")
     }
 
 
